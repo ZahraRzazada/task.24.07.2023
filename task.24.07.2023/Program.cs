8bdf69e1439d49f8054e7445be9723b6273a73a3
ZahraRzazada <zahraer@code.edu.az>
2023-07-25 00:39:45 +0400

    task.24.07.2023 was added

---
+++ task.24.07.2023/Program.cs
@@ -0,0 +1,33 @@
+﻿#region TASK2
+using task._24._07._2023;
+
+static void Check()
+{
+    int num1 = 34;
+    int num2 = 45;
+    int GivenNumber = 17;
+    int result1 = num1 - GivenNumber;
+    int result2 = num2 - GivenNumber;
+    if (Math.Abs(result1) > Math.Abs(result2))
+    {
+        Console.WriteLine("Num2 17 ye daha yaxindir");
+    }
+    else
+    {
+        Console.WriteLine("Num1 17 ye daha yaxindir");
+    }
+}
+Check();
+#endregion
+
+#region TASK2
+Student student = new Student("Zahra", "Rzazada", "ENG-30",false,96);
+student.Task3();
+student.Task2();
+student.Task4();
+
+
+
+#endregion
+
+
---
+++ task.24.07.2023/Student.cs
@@ -0,0 +1,52 @@
+﻿#region TASK1
+
+using System;
+namespace task._24._07._2023
+{
+	internal class Student
+    { 
+        public string Name;
+        public string SurName;
+        public string Group;
+        public bool IsGraduated;
+        public byte Point;
+
+    public Student(string name, string surName, string group, bool isGraduated, byte point)
+		{
+            Name = name;
+            SurName = surName;
+            Group = group;
+            IsGraduated = isGraduated;
+            Point = point;
+		}
+    public void Task3()
+        {
+            //Console.WriteLine(string.Concat(Name," ", SurName," ", Group," ", IsGraduated," ",));
+            Console.WriteLine($"Name of student:{Name},Surname of student:{SurName},Group of student:{Group},Is stdent graduated:{IsGraduated},Point of student:{Point}");
+            if (IsGraduated)
+            {
+                Console.WriteLine("This student was graduated");
+            }
+            else
+            {
+                Console.WriteLine("This student was not graduated");
+            }
+        }
+    public void Task2()
+        {
+            Console.WriteLine($"Name of student:{Name},Surname of student:{SurName}");
+        }
+    public void Task4()
+        {
+            if (Point > 80)
+            {
+                Console.WriteLine("This studen can participate in the next exam");
+            }
+            else
+            {
+                Console.WriteLine("This student can not participate in the next exam");
+            }
+        }
+	}
+}
+#endregion
